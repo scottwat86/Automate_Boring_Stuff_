70307f1ffbfe01edafd357b7ca973581e56bb965
[Scott Watson] <scottwat@gmail.com>
2019-06-28 23:36:33 -0700

    Ch 8

--- 1_Chapters/ch8.py
+++ 1_Chapters/ch8.py
@@ -67,3 +67,107 @@ os.path.basename(path) #'Automate_Boring_Stuff_'
 os.path.dirname(path) #'C:\\Users\\scott_watson\\Documents\\Python'
 
 os.path.split(path) # returns dirname and basename as seperate strings in a tuple
+os.path.split(os.path.sep) #('\\', '')
+
+#                   FINDING FILES SIZE AND FOLDER CONTENTS
+os.path.getsize(path) #40960  # size of file/directory in bytes
+
+os.listdir(path)
+r'''
+['.git',
+ '0_files',
+ '1_Chapters',
+ '2_video_lessons',
+ '3_practice_projects',
+ 'README.md'] '''
+
+os.path.getsize('README.md')  #376  bytes
+
+# Attribute Error
+# totalSize = 0
+# for filename in os.listdir('C:\\Windows\\System32'):
+#     totalSize = totalSize + os.path.getsize(os.path.joint('C:\\Windows\\System32', filename))
+# print(totalSize)
+
+#               CHECKING PATH VALIDITY
+os.path.exists('.') #True -> checks if the current working directory exists
+os.path.isfile(path) #False -> returns if arg exists and is a file
+os.path.isdir(path) # True -> returns if path arg exists and is a folder
+os.path.exists('C:\\Windows')
+os.path.exists('C:\\some_made_up+folder') #False
+os.path.isdir('C:\\Windows\\System32') # True
+os.path.isfile('C:\\Windows\\System32') #False
+
+os.path.exists('D:\\')
+
+#               FILE READING / WRITING PROCESS
+#               open() return a file object
+#               read() or write() method on the file object
+#               close() closes the file
+
+#               OPENING FILES WITH THE OPEN() FUNCTION
+helloFile = open('C:\\Users\\scott_watson\\Documents\\Python\\Automate_Boring_Stuff_\\1_Chapters\\hello_world.txt')
+helloFile.read() #'Hello World!'
+helloFile.close()
+
+#               READING THE CONTENTS OF FILES
+helloContent = helloFile.read()
+helloContent
+
+os.chdir('C:\\Users\\scott_watson\\Documents\\Python\\Automate_Boring_Stuff_\\1_Chapters\\')
+sonnetFile = open('sonnet29.txt')
+sonnetFile.readlines()
+'''
+["When, in disgrace with fortune and men's eyes,\n",
+ 'I all alone beweep my outcast state,\n',
+ 'And trouble deaf heaven with my bootless cries,\n',
+ 'And look upon myself and curse my fate,']
+'''
+
+
+#               WRITING TO FILES
+baconFile = open('bacon.txt', 'w')
+baconFile.write('Hellow world!\n')
+
+baconFile.close()
+baconFile = open('bacon.txt', 'a')
+baconFile.write('Bacon is not a vegetable.')
+
+baconFile.close()
+baconFile = open('bacon.txt')
+content = baconFile.read()
+baconFile.close()
+print(content)
+
+#               SAVING VARIABLES WITH THE SHELVE MODULE
+
+import shelve
+shelfFile = shelve.open('mydata')
+cats = ['Zophie', 'Pooka', 'Simon']
+shelfFile['cats'] = cats
+shelfFile.close()
+
+type(shelfFile) #shelve.DbfilenameShelf
+shelfFile['cats'] #['Zophie', 'Pooka', 'Simon']
+
+shelfFile = shelve.open('mydata')
+list(shelfFile.keys()) #['cats']
+list(shelfFile.values())
+
+#               SAVING VARIABLES WITH THE PPRINT.PFORMAT() FUNCTION
+
+import pprint
+cats = [{'name': 'Zophie', 'desc': 'chubby'}, {'name': 'Pooka', 'desc': 'fluffy'}]
+pprint.pformat(cats)
+"[{'desc': 'chubby', 'name': 'Zophie'}, {'desc': 'fluffy', 'name': 'Pooka'}]"
+
+fileObj = open('myCats.py', 'w')
+fileObj.write('cats = ' +pprint.pformat(cats) + '\n')
+fileObj.close()
+
+import myCats
+myCats.cats
+myCats.cats[0] #{'desc': 'chubby', 'name': 'Zophie'}
+myCats.cats[0]['name'] #'Zophie'
+
+#               PROJECT: GENERATING RANDOM QUIZ FILES
